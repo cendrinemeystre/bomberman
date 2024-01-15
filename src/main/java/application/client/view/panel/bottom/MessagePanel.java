c27ede01200c23cc588da042616c2931da946ce9
Cendrine Meystre <cme138579@stud.gibb.ch>
2024-01-15 23:41:23 +0100

    feat: small improvement

--- src/main/java/application/client/view/panel/bottom/MessagePanel.java
+++ src/main/java/application/client/view/panel/bottom/MessagePanel.java
@@ -6,18 +6,22 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MessagePanel extends Panel<BorderLayout> {
+    private final JScrollPane messageScrollPane;
     private final JTextArea messageTextArea;
 
     public MessagePanel() {
         super(new BorderLayout());
         messageTextArea = new JTextArea();
+        messageScrollPane = new JScrollPane(messageTextArea);
     }
 
     @Override
     public JPanel createPanel() {
         messageTextArea.setRows(20);
         messageTextArea.setEditable(false);
-        addToPanel(messageTextArea, null);
+        messageTextArea.setLineWrap(true);
+        messageTextArea.setWrapStyleWord(true);
+        addToPanel(messageScrollPane, null);
         JPanel panel = getPanel();
         panel.setEnabled(false);
         return panel;
